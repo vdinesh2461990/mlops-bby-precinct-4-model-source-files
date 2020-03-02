70768fa271da425f2519036e824ddc951450edb1
ec2-user <ec2-user@ip-172-31-29-109.ap-south-1.compute.internal>
2020-03-02 14:54:51 +0000

    Initial import

--- model/bby-precinct-4/MLFlowModel.py
+++ model/bby-precinct-4/MLFlowModel.py
@@ -8,7 +8,7 @@ class MLFlowModel(object):
     def __init__(self):
         
         
-        self.pyfunc_model = pyfunc.load_model("s3://ml-demo-dinesh/bestbuy/ba888f5eb30f410ba2090b1463abbb21/artifacts/model")
+        self.pyfunc_model = pyfunc.load_model("s3://ml-demo-dinesh/bestbuy/dd15d48ac01f454e91d22c7340ed56ce/artifacts/model")
 
     def predict(self,X,features_names):
         if not features_names is None and len(features_names)>0:
